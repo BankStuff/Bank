80d41406a896d2931f688abb5634c267089538ab
BankStuff <46943378+BankStuff@users.noreply.github.com>
2019-01-23 21:20:26 +0000

    New Features
    Withdrawals and deposits now work. Widgets have been relocated and more comments have been added for clarity.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import databases
 import tkinter
 from tkinter import messagebox
 import hashlib
+from decimal import Decimal
 
 # Globals
 
@@ -44,30 +45,128 @@ def saveBalance():
                 databases.writeto('balance',user,';','','.txt')
                 databases.writeto('balance',users[user],';','','.txt')
 
-def bankInfo(username, mainWindow):
+def withdraw(amount, funds, username, bankingWindow):
+
+        '''Withdraws money from the balance database'''
+
+        global users
+
+        # Validation
+        
+        try:
+                float(amount)
+                funds = float(funds)
+
+                if abs(Decimal(str(amount)).as_tuple().exponent) > 2:
+                        messagebox.showinfo('Withdrawal unsuccessful', 'Invalid fund entered')
+                        return None
+                elif abs(float(amount)) != float(amount):
+                        messagebox.showinfo('Withdrawal unsuccessful', 'Invalid fund entered')
+                        return None
+        except:
+                messagebox.showinfo('Withdrawal unsuccessful', 'Invalid fund entered')
+                return None
+
+        if not abs(float(funds) - float(amount)) == float(funds) - float(amount):
+                messagebox.showinfo('Withdrawal unsuccessful', 'Insufficent Funds')
+                return None
+
+        # Withdraws Money
+
+        users[username.upper()] = format(float(users[username.upper()]) - float(amount), '.2f')
+        saveBalance()
+        messagebox.showinfo('Withdrawal successful', 'Withdrawed a total of: £' + str(format(float(amount), '.2f')))
+
+        # Reloads Window
+
+        bankInfo(username, bankingWindow)
+
+def deposit(amount, funds, username, bankingWindow):
+
+        '''Deposits money from the balance database'''
+
+        global users
+
+        # Validation
+        
+        try:
+                float(amount)
+                funds = float(funds)
+
+                if abs(Decimal(str(amount)).as_tuple().exponent) > 2:
+                        messagebox.showinfo('Deposit unsuccessful', 'Invalid fund entered')
+                        return None
+                elif abs(float(amount)) != float(amount):
+                        messagebox.showinfo('Deposit unsuccessful', 'Invalid fund entered')
+                        return None
+        except:
+                messagebox.showinfo('Deposit unsuccessful', 'Invalid fund entered')
+                return None        
+
+        # Depsoits Money
+
+        users[username.upper()] = format(float(users[username.upper()]) + float(amount), '.2f')
+        saveBalance()
+        messagebox.showinfo('Deposit successful', 'Desposited a total of: £' + str(format(float(amount), '.2f')))
+
+        # Reloads Window
+
+        bankInfo(username, bankingWindow)
+
+def bankInfo(username, prevWindow):
         
         '''Creates the banking window'''
 
         global users
         
-        mainWindow.destroy() # Destroys Login Window
+        prevWindow.destroy() # Destroys Previous Window
 
         # Creates Banking Window
+
+        ## Window Configuration
         
         window = tkinter.Tk()
         window.title("Banking")
         window.geometry("250x250")
         window.configure(background='gray')
+
+        ## Balance And Logout Creations
+
         label3 = tkinter.Label(window, text = username.title() + "'s Balance: " + str(users[username.upper()]))
-        label3.place(x=125, y=80, anchor="center")
+        label3.place(x=125, y=20, anchor="center")
         logout = tkinter.Button(window, text='Logout',command=lambda: main(window))
-        logout.grid(row=0)
+        logout.grid()
+        logout.place(x=125, y=250, anchor="s")
+
+        ## Deposit Objects
+
+        entry4 = tkinter.Entry(window)
+        entry4.place(x=125, y=115, anchor="s")
+        label4 = tkinter.Button(window, text="Deposit:", command=lambda: deposit(entry4.get(), str(users[username.upper()]), username, window))
+        label4.place(x=125, y=95, anchor="s")
+
+        ## Withdrawal Objects
+
+        entry5 = tkinter.Entry(window)
+        entry5.place(x=125, y=175, anchor="s")
+        label5 = tkinter.Button(window, text="Withdraw:", command=lambda: withdraw(entry5.get(), str(users[username.upper()]), username, window))
+        label5.place(x=125, y=155, anchor="s")
+
+        ## Default Texts
+
+        entry4.insert(0, '0.00')
+        entry5.insert(0, '0.00')
+
+        ## Mainloop
+        
         window.mainloop()
 
 
 def userValidate(userLogin, userPass, mainWindow):
 
         '''Checks if the username and corrosponding password are valid'''
+
+        # Checks If The Usernames In The Database
         
         nameCorrect = False
 
@@ -77,6 +176,7 @@ def userValidate(userLogin, userPass, mainWindow):
                 messagebox.showinfo('FAILURE','Incorrect Username')
                 nameCorrect = True
 
+        # Checks If The Corrosponding Password Is Correct
         
         if nameCorrect == False:
                 if usernames[userLogin.upper()] == hash(userPass):
@@ -98,15 +198,17 @@ def main(root=None):
         getBalance()
 
         # Creates Login Window
+
+        ## Window Configuration
         
         window = tkinter.Tk()
         window.title('Login')
         window.geometry("200x150")
         window.configure(background='grey')
 
+        ## Object Creations
+
         label1 = tkinter.Label(window, text="Username:")
-
-
         entry1 = tkinter.Entry(window)
 
 
@@ -115,11 +217,24 @@ def main(root=None):
 
         login = tkinter.Button(window, text='Login', command=lambda: userValidate(entry1.get(), entry2.get(), window))
 
+        ## Object Placements
+
         label1.pack()
+        label1.place(x=100, y=35, anchor="s")
+        
         entry1.pack()
+        entry1.place(x=100, y=55, anchor="s")
+        
         label2.pack()
+        label2.place(x=100, y=85, anchor="s")
+        
         entry2.pack()
+        entry2.place(x=100, y=105, anchor="s")
+        
         login.pack()
+        login.place(x=100, y=145, anchor="s")
+
+        ## Mainloop
 
         window.mainloop()
 
